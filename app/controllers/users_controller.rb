37b270839ed136427dce137c33901e0bcbda493a
Hiroki Sato <resumi30minutes@gmail.com>
2023-09-06 15:00:26 +0900

    use index action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController < ApplicationController
-  
-  def show 
+  def index
+    @users = User.all
+  end
+  def show
     @user = User.find(params[:id])
   end
-  
+
   def new
     @user = User.new
   end
@@ -11,17 +13,17 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      reset_session 
+      reset_session
       log_in @user
       flash[:success] = "Welcomet o "
       redirect_to @user
     else
-      render 'new', status: :unprocessable_entity 
+      render 'new', status: :unprocessable_entity
     end
   end
 
-  private 
-  def user_params 
+  private
+  def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
