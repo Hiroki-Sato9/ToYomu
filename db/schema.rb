b95db44697ede785e076c76a41db900e382a492b
Hiroki Sato <resumi30minutes@gmail.com>
2023-09-06 15:37:39 +0900

    relate Goal model to User and Book

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_04_082713) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_06_061645) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "isbn"
+  end
+
+  create_table "goals", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "book_id", null: false
+    t.datetime "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_goals_on_book_id"
+    t.index ["user_id"], name: "index_goals_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -28,4 +38,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_04_082713) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "goals", "books"
+  add_foreign_key "goals", "users"
 end
