252440777ae72fc72b10b7513021de47d3809760
Hiroki Sato <resumi30minutes@gmail.com>
2023-09-07 21:05:02 +0900

    goal create action

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -4,9 +4,23 @@ class GoalsController < ApplicationController
   end
 
   def create
+    # ログインされている必要がある
     year = params[:goal]["date(1i)"]
     month = params[:goal]["date(2i)"]
     day = params[:goal]["date(3i)"]
     date = Time.zone.parse("#{year}-#{month}-#{day}")
+
+    book = Book.find_by(isbn: params[:goal][:isbn])
+    # 本がないとき
+    if book.nil?
+      redirect_to new_book_path and return
+    end
+
+    @goal = Goal.new(user_id: current_user.id, book_id: book.id, date: date)
+    if @goal.save
+      redirect_to current_user
+    else
+      #
+    end
   end
 end
